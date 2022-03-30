ba6b0af7603a7bfdc309b9d34879e87da0dab892
DomingoMango <l21121601@morelia.tecnm.mx>
2022-03-30 12:00:36 -0600

    Mwahahahah

---
+++ src/main/java/Tienda.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Tienda {
+    private String nombre;
+    private ArrayList<Objeto> objetosDisponibles;
+    public boolean comprar(double dinero, int cantidad, Objeto objeto){
+
+        return false;
+    }
+    public boolean vender(){
+
+        return false;
+    }
+}
