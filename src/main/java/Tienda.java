8d7fc7a1d2a7661c8080f1a21dce0adea85df4f2
Diego <l21121601@morelia.tecnm.mx>
2022-05-02 00:00:02 -0500

    sí

--- src/main/java/Tienda.java
+++ src/main/java/Tienda.java
@@ -4,6 +4,9 @@ public class Tienda {
     private String nombre;
     private ArrayList<Item> objetosDisponibles;
     public boolean comprar(double dinero, int cantidad, Item objeto){
+        //no recuerdo del todo asi que tambien debo estos,
+        // pero mañana le pregunto y los hago
+        // aunque cuando vea esto creo que ya los abre hecho
 
         return false;
     }
