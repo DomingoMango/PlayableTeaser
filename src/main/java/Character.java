30d5db597922e6d1bfa2619cc087ee08b9e2c902
Diego <l21121601@morelia.tecnm.mx>
2022-05-01 23:25:33 -0500

    getters y setters

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,2 +1,30 @@
 public class Character {
+
+    protected String name;
+    protected int lvl;
+    protected char genre;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLvl() {
+        return lvl;
+    }
+
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
+    }
+
+    public char getGenre() {
+        return genre;
+    }
+
+    public void setGenre(char genre) {
+        this.genre = genre;
+    }
 }
